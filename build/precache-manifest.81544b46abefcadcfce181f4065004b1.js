6629ec617fe404b33d16725f17303b4dba2888d3
zhaoyun11 <zhaoyun11@meituan.com>
2020-07-20 20:51:43 +0800

    初始化版本

---
+++ build/precache-manifest.81544b46abefcadcfce181f4065004b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c46f7b96d62b5ef1ad3b373df8a60ada",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2ec10a046622f19e59a",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "./static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ec10a046622f19e59a",
+    "url": "./static/js/main.e70d1a47.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
